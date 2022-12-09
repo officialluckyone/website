e554bc4bb9e6734ce4fe500a357ceb761bb58534
officialluckyone <kuanghe.go@gmail.com>
2022-12-10 04:59:02 +0700

    Project Slider Completed

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
+
+var counter = 2;
+setInterval (function(){
+    document.getElementById('radio'+counter).checked = true;
+    counter++;
+    if (counter > 6){
+        counter = 1;
+    }
+},5000);
