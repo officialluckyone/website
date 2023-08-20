8d51af3fd76d5a899759123828b6ab4ee7d08f45
officialluckyone <kuanghe.go@gmail.com>
2023-08-20 16:45:51 +0700

    fixed mouse drag on mobile

--- script.js
+++ script.js
@@ -33,118 +33,47 @@ function reveal(){
 
 }
 
-// const expcontainer = document.querySelector('#pastexperience');
-// const expcards = document.querySelector('.experiencecards');
-
-// // Keep Track for Mouse Down
-// let isPressedDown = false;
-
-// // Cursor X Space
-// let cursorXSpace;
-
-// expcontainer.addEventListener("mousedown",(e)=>{
-//     isPressedDown = true;
-//     cursorXSpace = e.offsetX - expcards.offsetLeft;
-// });
-
-// window.addEventListener("mouseup",(e)=>{
-//     isPressedDown = false;
-// });
-
-// expcontainer.addEventListener("mousemove",(e)=>{
-//     if (!isPressedDown) return;
-//     e.preventDefault();
-//     expcards.style.left = `${e.offsetX - cursorXSpace}px`;
-//     boundCards();
-// });
-
-
-// expcontainer.addEventListener("touchdown",(e)=>{
-//     isPressedDown = true;
-//     cursorXSpace = e.clientX - expcards.offsetLeft;
-// });
-
-// window.addEventListener("touchup",(e)=>{
-//     isPressedDown = false;
-// });
-
-// expcontainer.addEventListener("touchmove",(e)=>{
-//     if (!isPressedDown) return;
-//     e.preventDefault();
-//     expcards.style.left = `${e.clientX - cursorXSpace}px`;
-//     boundCards();
-// });
-
 const expcontainer = document.querySelector('#pastexperience');
 const expcards = document.querySelector('.experiencecards');
 
-// Keep Track for Mouse Down or Touch Start
+// Keep Track for Mouse Down
 let isPressedDown = false;
+
+// Cursor X Space
 let cursorXSpace;
 
-// Function to handle both mouse and touch events
-function startDragging(e) {
+expcontainer.addEventListener("mousedown",(e)=>{
     isPressedDown = true;
+    cursorXSpace = e.offsetX - expcards.offsetLeft;
+});
 
-    if (e.type === 'mousedown') {
-        cursorXSpace = e.offsetX - expcards.offsetLeft;
-    } else if (e.type === 'touchstart') {
-        cursorXSpace = e.touches[0].clientX - expcards.getBoundingClientRect().left;
-    }
-}
-
-function stopDragging() {
+window.addEventListener("mouseup",(e)=>{
     isPressedDown = false;
-}
+});
 
-function handleDragging(e) {
+expcontainer.addEventListener("mousemove",(e)=>{
     if (!isPressedDown) return;
     e.preventDefault();
-
-    let clientX;
-    if (e.type === 'mousemove') {
-        clientX = e.clientX;
-    } else if (e.type === 'touchmove') {
-        clientX = e.touches[0].clientX;
-    }
-
-    expcards.style.left = `${clientX - cursorXSpace}px`;
+    expcards.style.left = `${e.offsetX - cursorXSpace}px`;
     boundCards();
-}
-
-expcontainer.addEventListener("mousedown", startDragging);
-expcontainer.addEventListener("touchstart", startDragging);
-
-window.addEventListener("mouseup", stopDragging);
-window.addEventListener("touchend", stopDragging);
-
-expcontainer.addEventListener("mousemove", handleDragging);
-expcontainer.addEventListener("touchmove", handleDragging);
-
-function boundCards() {
-    const container_rect = expcontainer.getBoundingClientRect();
-    const cards_rect = expcards.getBoundingClientRect();
-
-    if (parseInt(expcards.style.left) > 0) {
-        expcards.style.left = 0;
-    } else if (cards_rect.right < container_rect.right) {
-        expcards.style.left = `-${cards_rect.width - container_rect.width}px`;
-    }
-}
+});
 
 
+expcontainer.addEventListener("touchdown",(e)=>{
+    isPressedDown = true;
+    cursorXSpace = e.clientX - expcards.offsetLeft;
+});
 
+window.addEventListener("touchup",(e)=>{
+    isPressedDown = false;
+});
 
-function boundCards(){
-    const container_rect = expcontainer.getBoundingClientRect();
-    const cards_rect = expcards.getBoundingClientRect();
-
-    if (parseInt(expcards.style.left) > 0){
-        expcards.style.left = 0;
-    } else if (cards_rect.right < container_rect.right){
-        expcards.style.left = `-${cards_rect.width - container_rect.width}px`;
-    }
-};
+expcontainer.addEventListener("touchmove",(e)=>{
+    if (!isPressedDown) return;
+    e.preventDefault();
+    expcards.style.left = `${e.clientX - cursorXSpace}px`;
+    boundCards();
+});
 
 // Mobile Drag & Drop Code
 // Position Variable
